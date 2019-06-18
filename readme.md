50702927851c99cc7b0d6f9f35cad1485c3f5616
koechkevin <koechkevin92@gmail.com>
2019-06-18 22:05:28 +0300

    set up react redux sass coveralls and travis CI

---
+++ readme.md
@@ -0,0 +1,3 @@
+#### random-phone-number-generator
+[![Build Status](https://travis-ci.org/koechkevin/random-phone-number-generator.svg?branch=master)](https://travis-ci.org/koechkevin/random-phone-number-generator)
+[![Coverage Status](https://coveralls.io/repos/github/koechkevin/random-phone-number-generator/badge.svg?branch=master)](https://coveralls.io/github/koechkevin/random-phone-number-generator?branch=master)
---
+++ src/views/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import store from '../redux/store';
+import '../styles/styles.scss'
+
+const history = createBrowserHistory();
+
+const App = () => (
+  <Provider store={store}>
+    <Router history={history}>
+      This is an app
+    </Router>
+  </Provider>
+);
+export default App;
