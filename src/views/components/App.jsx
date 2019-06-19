1385b990c4399509abd9e385bcfa583f01a1ae14
koechkevin <koechkevin92@gmail.com>
2019-06-19 10:49:15 +0300

    set up a component

--- src/views/components/App.jsx
+++ src/views/components/App.jsx
@@ -3,14 +3,17 @@ import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import store from '../redux/store';
-import '../styles/styles.scss'
+import Home from './Home';
+import NavBar from './NavBar';
+import '../styles/styles.scss';
 
 const history = createBrowserHistory();
 
 const App = () => (
   <Provider store={store}>
     <Router history={history}>
-      This is an app
+      <NavBar />
+      <Home />
     </Router>
   </Provider>
 );
